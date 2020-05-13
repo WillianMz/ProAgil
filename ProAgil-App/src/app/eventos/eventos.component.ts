a5c151e2537591c8d9c3196536d90d05838a3a8a
Willian Mz <willianmazzorana@hotmail.com>
2020-05-12 23:57:25 -0300

    Inclusão dos conceitos de Interpolação, Diretivas e Binding

--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,20 +8,50 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
-  Eventos: any;
+  listaFiltro: string;
+  get filtroLista(): string
+  {
+    return this.listaFiltro;
+  }
+
+  set filtroLista(value: string)
+  {
+    this.listaFiltro = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEvento(this.filtroLista) : this.Eventos;
+  }
+
+  eventosFiltrados: any = [];
+
+  Eventos: any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
 
   constructor(private http: HttpClient) { }
 
-  ngOnInit() 
+  ngOnInit()
   {
     this.getEventos();
+  }
+
+  filtrarEvento(filtrarPor: string)
+  {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.Eventos.filter(evento =>
+      {
+        return evento.tema.toLocaleLowerCase().includes(filtrarPor);
+      });
+  }
+
+  alternarImagem()
+  {
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
   getEventos()
   {
     this.http.get('http://localhost:5000/api/values').subscribe(
-      response => { this.Eventos = response; console.log(response)},
-      error => {console.log(error);}
+      response => { this.Eventos = response; console.log(response); }, error => { console.log(error); }
     );
   }
 
